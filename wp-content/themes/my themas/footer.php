d7baef0a0ddd545d1fd086ea522181bc4fb32a99
Artur <jakord@mail.ru>
2015-07-31 17:53:53 +0300

    изменный футер

--- wp-content/themes/my themas/footer.php
+++ wp-content/themes/my themas/footer.php
@@ -1,6 +1,6 @@
-     <div class="row">
+     
      <footer>
-
+       <div class="row">
         <div class=" col-sm-12 footer-up">
            <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
            <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
@@ -13,8 +13,9 @@
           <span>&copy;Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span>
           <span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span>
        </div>
+       </div>
      </footer>
-     </div>
+     
      
      </div>
      <!-- end container -->
