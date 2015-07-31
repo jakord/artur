3153f534a4c23e451f41c8b0d6f75eb186a7f384
Artur <jakord@mail.ru>
2015-07-31 16:39:17 +0300

    wp-content/themes/my themas/header.php

--- wp-content/themes/my themas/footer.php
+++ wp-content/themes/my themas/footer.php
@@ -1,23 +1,31 @@
-    <div class="foooter">
-        <div class="col-sm-12 prefooter">
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class="col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+     <div class="row">
+     <footer>
+
+        <div class=" col-sm-12 footer-up">
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
         </div>
-       <div class="col-sm-12  footer">
+       <div class="col-sm-12  footer-down">
           <span>&copy;Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span>
-          <span class="pull-right"><a href="https://www.facebook.com"><img  src="<?php bloginfo('template_url') ?>/image/fb.png" alt=""></a>&nbsp;<a href="https://twitter.com/?lang=ru"><img  src="<?php bloginfo('template_url') ?>/image/twt.png" alt="#"></a></span>
+          <span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span>
        </div>
+     </footer>
      </div>
+     
      </div>
-    
-   </div> 
+     <!-- end container -->
       
+   </div> 
+      <!-- end wrapper -->
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-	<?php wp_footer();?>
+    <script src="js/bootstrap.js"></script>
+    <?php wp_footer();?>
   </body>
 </html>
--- wp-content/themes/my themas/header.php
+++ wp-content/themes/my themas/header.php
@@ -4,81 +4,45 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>SiteName</title>
+
     <!-- Bootstrap -->
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script  src="<?php bloginfo('template_url') ?>/https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script  src="<?php bloginfo('template_url') ?>/https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	<?php wp_head();?>
+    <?php wp_head();?>
   </head>
   <body>
       
     <div class="wrapper">
-
        <div class="container">
-         <div class="header">
-         
-          <nav class="navbar navbar-default">
+
+         <header>
+          <nav class="row navbar  site-navbar navbar-default">
               <div class="col-md-12 col-lg-4">
                <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
+                  <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?php echo home_url();?>"><img class="img-responsive"   src="<?php bloginfo('template_url') ?>/image/logo.png"> </a>
+                <div class="col-sm-12">
+                  <a class="navbar-brand site-brand" href="#"><img class="img-responsive" src="image/logo.png" alt="logotype"> </a>
+                </div>  
               </div>  
 
               </div>
-
-
-
-               <div class="collapse navbar-collapse">
-               <div class=" col-lg-8 visible-lg">
-                
-                   <ul class="nav navbar-nav pull-right">
-                         <li><a href="#">Home</a></li>
-                         <li><a href="#">About</a></li>
-                         <li><a href="#">Services</a></li>
-                         <li><a href="#">Portfolio</a></li>
-                         <li><a href="#">Contact</a></li> 
-                   </ul>
-                </div>
-              </div>
-
-               <div class=" col-md-12 visible-md ">
-                 <div class="collapse navbar-collapse">
-                   <ul class="nav navbar-nav ">
-                         <li><a href="#">Home</a></li>
-                         <li><a href="#">About</a></li>
-                         <li><a href="#">Services</a></li>
-                         <li><a href="#">Portfolio</a></li>
-                         <li><a href="#">Contact</a></li> 
-                   </ul>
-                </div>
-              </div>
- 
-              <div class="collapse navbar-collapse">
-              <div class=" col-sm-12 visible-sm">
-                 
-                   <ul class="nav navbar-nav ">
-                         <li><a href="#">Home</a></li>
-                         <li><a href="#">About</a></li>
-                         <li><a href="#">Services</a></li>
-                         <li><a href="#">Portfolio</a></li>
-                         <li><a href="#">Contact</a></li> 
-                   </ul>
-                </div>
-              </div>
               <div class="collapse navbar-collapse" id="navbar-collapse">
-              <div class=" col-sm-12 visible-xs">
-                 
-                   <ul class="nav navbar-nav ">
+              <div class="row">
+              <div class=" col-lg-7 col-lg-offset-1  col-md-10 col-sm-12 ">
+                   <ul class="nav site-nav navbar-nav site-navbar-nav ">
                          <li><a href="#">Home</a></li>
                          <li><a href="#">About</a></li>
                          <li><a href="#">Services</a></li>
@@ -86,26 +50,17 @@
                          <li><a href="#">Contact</a></li> 
                    </ul>
                 </div>
+                </div>
               </div>
-              
-
-            <!--начало выпадаешего меню-->
-            
-           
-            <!--конец выпадаешего меню-->   
            </nav>
         
-          
-          <div class="col-md-12 baner">
-          
-          <!-- <div class="col-sm-5 "><img class="img-responsive banner"   src="<?php bloginfo('template_url') ?>/image/baner.png" alt="banner"></div>  -->
-         
-          <div class="col-md-7 col-sm-12 pull-right">
-             <h3 class="baner-h3">Lorem ipsum dolor sit amet</h3>
-             <h1 class="baner-h1">Donec nec justo eget</h1>
-             <p class="baner-p">Morbi in sem quis dui placerat ornare. Pellentesque odioeuismod in, pharetra a, ultricies in, diam.</p>
-             <a href="#" class="baner-a"><button type="button btn-lg" class="btn btn-danger">More Info</button></a>
-          </div>
-       </div>
 
-      </div>
+              <div class="row">
+              <div class=" col-md-12 baner">
+                    <div class="row col-md-7 row col-sm-12 pull-right">
+                      <h3 >Lorem ipsum dolor sit amet</h3><h1 >Donec nec justo eget</h1><p >Morbi in sem quis dui placerat ornare. Pellentesque odioeuismod in, pharetra a, ultricies in, diam.</p><span> <button type="button" class="btn site-btn btn-lg btn-danger">More Info</button> </span>
+                    </div>
+              </div>
+              </div>
+
+            </header> 
