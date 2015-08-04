e36639ec67d84a36ef60bd31567ef39095c51d63
Artur <jakord@mail.ru>
2015-08-04 16:12:09 +0300

    изменил структуру футера

--- wp-content/themes/my themas/footer.php
+++ wp-content/themes/my themas/footer.php
@@ -1,19 +1,29 @@
-     
-     <footer>
-       <div class="row">
-        <div class=" col-sm-12 footer-up">
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-           <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+      <footer>
+
+         <div class="row">
+          <div class="col-sm-12">
+            <div class="row footer-up">
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+            </div>
+          </div>
+        </div>  
+
+
+
+        <div class="row">
+          <div class="col-sm-12  footer-down">
+            <div class="row">
+              <div class="col-sm-9"><span>&copy;Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span></div>
+              <div class="col-sm-3 pull-right hidden-xs "><span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span></div>
+            </div>
+           </div> 
         </div>
-       <div class="col-sm-12  footer-down">
-          <span>&copy;Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span>
-          <span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span>
-       </div>
-       </div>
+
      </footer>
      
      
@@ -22,11 +32,6 @@
       
    </div> 
       <!-- end wrapper -->
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.js"></script>
     <?php wp_footer();?>
   </body>
 </html>
