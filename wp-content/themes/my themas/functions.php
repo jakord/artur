c72b26afe05f788038352f66ebfcab180a54204a
Artur <jakord@mail.ru>
2015-08-04 16:14:58 +0300

    изменил структуру футера

--- wp-content/themes/my themas/functions.php
+++ wp-content/themes/my themas/functions.php
@@ -7,9 +7,10 @@ function load_style_script(){
 	wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700');
 	wp_enqueue_style('bootstrap_style', get_template_directory_uri() . '/css/bootstrap.css');
 	wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-	wp_enqueue_script('jquery-google', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
-	wp_enqueue_script('style-jquery-ui-1.9.2', get_template_directory_uri() . '/js/bootstrap.js');
+        wp_enqueue_script('jquery-library', get_template_directory_uri() . '/js/jquery1-11min.js');
+	wp_enqueue_script('style-jquery', get_template_directory_uri() . '/js/bootstrap.js');
 }
+
 
 /**
 * загружаем скрипты и стили
