dd5b7d41a4cb1536146eda36c4d182545cefc90f
Artur <jakord@mail.ru>
2015-08-06 11:32:27 +0300

    I changed footer structure and remowe the some classes

--- wp-content/themes/my themas/footer.php
+++ wp-content/themes/my themas/footer.php
@@ -1,29 +1,17 @@
-      <footer>
+ <footer>
+         <div class="row footer-up">
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
+         </div>
 
-         <div class="row">
-          <div class="col-sm-12">
-            <div class="row footer-up">
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-               <div class=" col-md-2 col-sm-4"><h3>Vitae lin</h3><p>Phasellus ultricesSed adipiscing lipsumNulla sed leoniton</p></div>
-            </div>
-          </div>
-        </div>  
-
-
-
-        <div class="row">
-          <div class="col-sm-12  footer-down">
-            <div class="row">
-              <div class="col-sm-9"><span>&copy;Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span></div>
-              <div class="col-sm-3 pull-right hidden-xs "><span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span></div>
-            </div>
-           </div> 
-        </div>
-
+         <div class="row footer-down ">
+             <div class="col-sm-9"><span>Sitename. All rights Reserved. Designed By www.alltemplateneeds.com / Photos from : www.photorack.net</span></div>
+             <div class="col-sm-3"><span class="pull-right hidden-xs"><a href=""><img src="image/fb.png" alt=""></a>&nbsp;<a href="#"><img src="image/twt.png" alt="#"></a></span></div>
+        </div> 
      </footer>
      
      
