725263ea522ec0ecea9b68714d6b4ac46da5aab8
Artur <jakord@mail.ru>
2015-08-06 11:37:40 +0300

    I changed index.php structure

--- wp-content/themes/my themas/index.php
+++ wp-content/themes/my themas/index.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
-<div class="row">
-            <div class="col-sm-12 row1">
-                <div class="row">
+<div class="row portfolio">
                    <div class=" col-sm-4">  
                     <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/flag.png" alt="flag"></div>
-                        <div class="row1-article-right">
+                        <div class="portfolio-article-right">
                         <h5 >Ausce Cellentes</h5>
                         <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
                         <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
@@ -13,7 +11,7 @@
 
                    <div class=" col-sm-4">  
                     <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/fo.png" alt="foto"></div>
-                        <div class="row1-article-right">
+                        <div class="portfolio-article-right">
                         <h5 >Ausce Cellentes</h5>
                         <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
                         <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
@@ -22,36 +20,32 @@
 
                    <div class=" col-sm-4">  
                     <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/hs.png" alt="house"></div>
-                        <div class="row1-article-right">
+                        <div class="portfolio-article-right">
                         <h5 >Ausce Cellentes</h5>
                         <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
                         <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
                         </div>
                    </div>
 
-                 </div>
-            </div>
-            </div>
+            </div> 
 
   
 
-             <div class="row">
-     <div class=" col-sm-12 row2">
-      <div class="row">
+           <div class="row row2">
           <div class="col-sm-8">
            <div class="row article">
-             <div class="col-sm-4 col-xs-4"><img src="image/img1.png" alt="image" class="<?php bloginfo('template_url')?>/img-rounded img-responsive" > </div>
-             <div class="col-sm-8  col-xs-8 row"><h5 class="header">Lorem ipsum dolor sit amet</h5> 
+             <div class="col-sm-4 col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img1.png" alt="image" class="img-rounded img-responsive" > </div>
+             <div class="col-sm-8  col-xs-8 "><h5 class="header">Lorem ipsum dolor sit amet</h5>
              <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p> 
-              <a href="#"  class="btn btn-danger btn-lg">More Info</a> 
+              <a href="#"  class="btn btn-danger ">More Info</a>
              </div>
              </div>
 
              <div class="row article">
-             <div class="col-sm-4  col-xs-4"><img src="image/img2.png" alt="image" class="<?php bloginfo('template_url')?>/img-rounded img-responsive" > </div>
-             <div class="col-sm-8 col-xs-8 row"><h5>Lorem ipsum dolor sit amet</h5> 
+             <div class="col-sm-4  col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img2.png" alt="image" class="img-rounded img-responsive" > </div>
+             <div class="col-sm-8 col-xs-8"><h5>Lorem ipsum dolor sit amet</h5>
              <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p>
-             <a href="#"  class="btn btn-danger btn-lg">More Info</a> 
+             <a href="#"  class="btn btn-danger">More Info</a>
              </div>
           </div>
           </div>
@@ -77,91 +71,66 @@
                 </div>
              </div>
           </div>
-        </div>  
-
-    </div> 
-    </div>
+         </div>
      
 
 
       <div class="row">
-        <div class="col-sm-12 text-center row3 ">
+        <div class="text-center row3 ">
            <h3>Praesent Clacerat</h3>
            <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreetlectus arcu pulvinar risus</p>  
       </div>
-      </div>
+      </div> 
 
    
        <div class="row row4">
 
                 <h2 class="text-center">Vestibulum Commodo Felis</h2>
                 <h3 class="text-center">Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna</h3>
-          
-                   
-           <div class=" col-md-12 ">
-            
+
                 <div class="col-md-6">
-                 <div class="row">
-                    <div class="col-sm-12  row4-left-article-bottom">
-                    <div class="row">
-                      <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>image/kl1.png" alt="kalendar"></div>
-                       <div class="col-sm-10 col-xs-10 row4-left-article"><h5 >Gravida Ultrice</h5><span>14 Comments</span> <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p></div></div>
+                    <div class="row row4-left-article-bottom">
+                   <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
+                    <div class="col-sm-10 col-xs-10 row4-left-article">
+                        <h5 >Gravida Ultrice</h5>
+                        <span>14 Comments</span>
+                        <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
                     </div>
-                  </div>  
-
-                   <div class="row">
-                    <div class="col-sm-12  row4-left-article-bottom">
-                    <div class="row">
-                      <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>image/kl1.png" alt="kalendar"></div>
-                       <div class="col-sm-10 col-xs-10 row4-left-article"><h5 >Gravida Ultrice</h5><span>14 Comments</span> <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p></div></div>
                     </div>
-                  </div>
-                  
-
-                     <div class="row">
-                    <div class="col-sm-12  row4-left-article-bottom">
-                    <div class="row">
-                      <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>image/kl1.png" alt="kalendar"></div>
-                       <div class="col-sm-10 col-xs-10 row4-left-article"><h5 >Gravida Ultrice</h5><span>14 Comments</span> <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p></div></div>
+                    <div class="row row4-left-article-bottom">
+                        <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
+                        <div class="col-sm-10 col-xs-10 row4-left-article">
+                            <h5 >Gravida Ultrice</h5>
+                            <span>14 Comments</span>
+                            <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
+                        </div>
                     </div>
-                  </div>
-
+                    <div class="row row4-left-article-bottom">
+                        <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
+                        <div class="col-sm-10 col-xs-10 row4-left-article">
+                            <h5 >Gravida Ultrice</h5>
+                            <span>14 Comments</span>
+                            <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
+                        </div>
+                    </div>
 
                 </div>        
                  
-            
+
                 <div class=" col-md-6 ">
+                <div class="row article-bottom ">
+                <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
+                    <span>- John Doe</span>
+                </div><div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
+                </div>
 
+                <div class="row article-bottom ">
+                  <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
+                            <span>- John Doe</span></div>
+                  <div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
+                 </div>
 
-                   <div class="row">
-                     <div class="col-sm-12   article-bottom">
-                      <div class="row">
-                        <div class="col-sm-9 col-xs-10 row4-right-article">Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ul.
-                        <br/><span>- John Doe</span>
-                      </div>
-                       <div class="col-sm-3 col-xs-2 "><img src="<?php bloginfo('template_url')?>image/qs.png" class="img-responsive"  alt=""></div>
-                     </div> 
-                      </div>    
-                   </div>   
-
-                <div class="row">
-                     <div class="col-sm-12   article-bottom">
-                      <div class="row">
-                        <div class="col-sm-9 col-xs-10 row4-right-article">Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ul.
-                        <br/><span>- John Doe</span>
-                      </div>
-                       <div class="col-sm-3 col-xs-2 "><img src="<?php bloginfo('template_url')?>image/qs.png" class="img-responsive"  alt=""></div>
-                     </div> 
-                      </div>    
-                   </div> 
-
-
-                    </div>
-                     
-              
-            
-             </div> 
-             
-         </div>
+                </div>     
+      </div>
           
  <?php get_footer();?>
