b91b7a4b221855d882876019eaa46fca3b65e04f
Artur <jakord@mail.ru>
2015-08-07 15:11:20 +0300

    comment out unnecessary

--- wp-content/themes/my themas/index.php
+++ wp-content/themes/my themas/index.php
@@ -32,8 +32,10 @@
   
 
            <div class="row row2">
-          <div class="col-sm-8">
-           <div class="row article">
+
+           <div class="col-sm-8">
+
+            <div class="row article">
              <div class="col-sm-4 col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img1.png" alt="image" class="img-rounded img-responsive" > </div>
              <div class="col-sm-8  col-xs-8 "><h5 class="header">Lorem ipsum dolor sit amet</h5>
              <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p> 
@@ -41,13 +43,14 @@
              </div>
              </div>
 
-             <div class="row article">
+            <!--  <div class="row article">
              <div class="col-sm-4  col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img2.png" alt="image" class="img-rounded img-responsive" > </div>
              <div class="col-sm-8 col-xs-8"><h5>Lorem ipsum dolor sit amet</h5>
              <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p>
              <a href="#"  class="btn btn-danger">More Info</a>
              </div>
-          </div>
+          </div> -->
+
           </div>
 
           <div class="col-sm-4">
@@ -75,11 +78,9 @@
      
 
 
-      <div class="row">
-        <div class="text-center row3 ">
+      <div class="row text-center row3">
            <h3>Praesent Clacerat</h3>
            <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreetlectus arcu pulvinar risus</p>  
-      </div>
       </div> 
 
    
@@ -89,6 +90,7 @@
                 <h3 class="text-center">Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna</h3>
 
                 <div class="col-md-6">
+
                     <div class="row row4-left-article-bottom">
                    <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
                     <div class="col-sm-10 col-xs-10 row4-left-article">
@@ -97,38 +99,41 @@
                         <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
                     </div>
                     </div>
-                    <div class="row row4-left-article-bottom">
+
+                   <!--  <div class="row row4-left-article-bottom">
                         <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
                         <div class="col-sm-10 col-xs-10 row4-left-article">
                             <h5 >Gravida Ultrice</h5>
                             <span>14 Comments</span>
                             <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
                         </div>
-                    </div>
-                    <div class="row row4-left-article-bottom">
+                    </div> -->
+                    
+                    <!-- <div class="row row4-left-article-bottom">
                         <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
                         <div class="col-sm-10 col-xs-10 row4-left-article">
                             <h5 >Gravida Ultrice</h5>
                             <span>14 Comments</span>
                             <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
                         </div>
-                    </div>
+                    </div> -->
 
                 </div>        
                  
 
                 <div class=" col-md-6 ">
+
                 <div class="row article-bottom ">
                 <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
                     <span>- John Doe</span>
                 </div><div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
                 </div>
 
-                <div class="row article-bottom ">
+              <!--   <div class="row article-bottom ">
                   <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
                             <span>- John Doe</span></div>
                   <div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
-                 </div>
+                 </div> -->
 
                 </div>     
       </div>
