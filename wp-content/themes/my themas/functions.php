0b1404055e3f4252c941b65a3f6c40bebd9190cd
Artur <jakord@mail.ru>
2015-08-10 18:14:02 +0300

    mplementation wordpress theme

--- wp-content/themes/my themas/functions.php
+++ wp-content/themes/my themas/functions.php
@@ -1,24 +1,30 @@
 <?php
-
-/**
-* загружаемые скрипты и стили
-*/
 function load_style_script(){
 	wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700');
 	wp_enqueue_style('bootstrap_style', get_template_directory_uri() . '/css/bootstrap.css');
 	wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-        wp_enqueue_script('jquery-library', get_template_directory_uri() . '/js/jquery1-11min.js');
-	wp_enqueue_script('style-jquery', get_template_directory_uri() . '/js/bootstrap.js');
+	wp_enqueue_script('library-jquery', get_template_directory_uri() . '/js/jquery-1.11.3.min.js');
+	wp_enqueue_script('bootstrap-jquery', get_template_directory_uri() . '/js/bootstrap.js');
+}
+
+add_action('wp_enqueue_scripts', 'load_style_script');
+
+
+register_nav_menus(array(
+	'header_menu' => 'Меню в шапке',
+));
+add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu' );
+function my_wp_nav_menu( $args = '' ){
+	$args['container'] = false;
+	return $args;
 }
 
 
-/**
-* загружаем скрипты и стили
-*/
-add_action('wp_enqueue_scripts', 'load_style_script');
+add_theme_support('post-thumbnails');
 
-/**
-* регистрируем меню
-**/
-	
-?>
+register_sidebar(array(
+	'name' => 'footer',
+	'id' => 'footer',
+	'before_widget' => '',
+	'after_widget' => ''
+	));
