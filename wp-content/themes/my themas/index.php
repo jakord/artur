9783106331db5bbcf6a2087fa916f45f2fd383f0
Artur <jakord@mail.ru>
2015-08-10 18:09:26 +0300

    implementation wordpress theme

--- wp-content/themes/my themas/index.php
+++ wp-content/themes/my themas/index.php
@@ -1,141 +1,114 @@
 <?php get_header(); ?>
-<div class="row portfolio">
-                   <div class=" col-sm-4">  
-                    <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/flag.png" alt="flag"></div>
-                        <div class="portfolio-article-right">
-                        <h5 >Ausce Cellentes</h5>
-                        <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
-                        <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
-                        </div>
-                   </div>
+<?php
+$cat_id=4;
+$portfolio=  new WP_Query(array('cat'=>$cat_id,'posts_per_page' =>3));
+?>
+        <?php  if ( $portfolio->have_posts() ) :  ?>
+        <div class="row portfolio">
+            <?php while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
+                <div class=" col-sm-4">
+                    <div class=" pull-left"><?php the_post_thumbnail(); ?></div>
+                    <div class="portfolio-article-right">
+                        <a href="<?php the_permalink();?>" <h5 ><?php the_title();?></h5></a>
+                        <p ><?php the_excerpt()?></p>
+                        <a href="<?php the_permalink()?>" >read more &nbsp;<img src="<?php bloginfo('template_url') ?>/image/arrow.png" alt="arrow"></a>
+                    </div>
+                </div>
+        <?php endwhile; ?>
+        </div>
+            <?php else:?>
+        <?php endif; ?>
+<div class="row row2">
 
-                   <div class=" col-sm-4">  
-                    <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/fo.png" alt="foto"></div>
-                        <div class="portfolio-article-right">
-                        <h5 >Ausce Cellentes</h5>
-                        <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
-                        <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
-                        </div>
-                   </div>
+    <?php
+    $cat_id=3;
+    $services=  new WP_Query(array('cat'=>$cat_id,'posts_per_page' =>2)); ?>
+    <?php  if ( $services->have_posts() ) :  ?>
+    <div class="col-sm-8">
+        <?php while ( $services->have_posts() ) : $services->the_post(); ?>
+        <div class="row article">
+            <div class="col-sm-4 col-xs-4"><?php the_post_thumbnail(true,array('class' => "img-rounded img-responsive")); ?></div>
+            <div class="col-sm-8  col-xs-8 "><h5 class="header"><?php the_title();?></h5>
+                <p><?php the_excerpt()?></p>
+                <a href="<?php the_permalink()?>"  class="btn btn-danger ">More Info</a>
+            </div>
+        </div>
+        <?php endwhile; ?>
+    </div>
+    <?php else:?>
+    <?php endif; ?>
 
-                   <div class=" col-sm-4">  
-                    <div class=" pull-left"><img src="<?php bloginfo('template_url')?>/image/hs.png" alt="house"></div>
-                        <div class="portfolio-article-right">
-                        <h5 >Ausce Cellentes</h5>
-                        <p >Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique</p>
-                        <a href="#" >read more</a><img src="<?php bloginfo('template_url')?>/image/arrow.png" alt="arrow">
-                        </div>
-                   </div>
-
-            </div> 
-
-  
-
-           <div class="row row2">
-
-           <div class="col-sm-8">
-
-            <div class="row article">
-             <div class="col-sm-4 col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img1.png" alt="image" class="img-rounded img-responsive" > </div>
-             <div class="col-sm-8  col-xs-8 "><h5 class="header">Lorem ipsum dolor sit amet</h5>
-             <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p> 
-              <a href="#"  class="btn btn-danger ">More Info</a>
-             </div>
-             </div>
-
-            <!--  <div class="row article">
-             <div class="col-sm-4  col-xs-4"><img src="<?php bloginfo('template_url')?>/image/img2.png" alt="image" class="img-rounded img-responsive" > </div>
-             <div class="col-sm-8 col-xs-8"><h5>Lorem ipsum dolor sit amet</h5>
-             <p>sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus tpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu</p>
-             <a href="#"  class="btn btn-danger">More Info</a>
-             </div>
-          </div> -->
-
-          </div>
-
-          <div class="col-sm-4">
-             <div class="tabs">
+    <div class="col-sm-4">
+        <?php
+        $cat_id=7;
+        $eronded=  new WP_Query(array('cat'=>$cat_id,'posts_per_page' =>1));
+        ?>
+        <?php  if ( $eronded->have_posts() ) :  ?>
+            <div class="tabs">
                 <ul class=" nav nav-tabs site-nav-tabs">
-                   <li class="active"><a href="#tab" data-toggle="tab">Eronped</a></li>
-                   <li><a href="#tab1" data-toggle="tab">Centro</a></li> 
+                    <li class="active"><a href="#tab" data-toggle="tab">Eronped</a></li>
+                    <li><a href="#tab1" data-toggle="tab">Centro</a></li>
                 </ul>
-                <div class="tab-content site-tab-content">
-                   <div id="tab" class="tab-pane fade in active"><h4 class="tab-header">Sedegin libero commo</h4><p>Simasellus ultrices nulla quis nibh. Quisque a lectus. Donec coSimasellus ultrices nulla quis nibh. Quisque a lectus. Donec co</p>
-                     <span class="glyphicon glyphicon-arrow-right"><a href="#">istique elit  sinto sum</a></span>
-                     <span class="glyphicon glyphicon-arrow-right"><a href="#">istique elit  sinto sum </a></span>
-                     <span class="glyphicon glyphicon-arrow-right"><a href="#">istique elit  sinto sum </a></span>
-
-                   </div>
-
-                   <div id="tab1" class="tab-pane fade">
-                      <h4 class="tab-header">Sedegin libero commo</h4><p>Simasellus ultrices nulla quis nibh. Quisque a lectus. Donec coSimasellus ultrices nulla quis nibh. Quisque a lectus. Donec co</p>
-                   </div>
-                 
-                </div>
-             </div>
-          </div>
-         </div>
-     
+                <?php while ( $eronded->have_posts() ) : $eronded->the_post(); ?>
+                    <div class="tab-content site-tab-content">
+                        <div id="tab" class="tab-pane fade in active"><h4 class="tab-header"><?php the_title();?></h4><p><?php the_content()?></p>
+                            <span class="glyphicon glyphicon-arrow-right"><a href="<?php the_permalink();?>">istique elit  sinto sum</a></span>
+                        </div>
+                        <div id="tab1" class="tab-pane fade">
+                            <h4 class="tab-header"><?php the_title()?></h4><p><?php the_content()?></p>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        <?php else:?>
+        <?php endif; ?>
+    </div>
+</div>
 
 
-      <div class="row text-center row3">
-           <h3>Praesent Clacerat</h3>
-           <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreetlectus arcu pulvinar risus</p>  
-      </div> 
-
-   
-       <div class="row row4">
-
-                <h2 class="text-center">Vestibulum Commodo Felis</h2>
-                <h3 class="text-center">Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna</h3>
-
-                <div class="col-md-6">
-
-                    <div class="row row4-left-article-bottom">
-                   <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
+<div class="row">
+    <div class="text-center row3 ">
+        <h3>Praesent Clacerat</h3>
+        <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreetlectus arcu pulvinar risus</p>
+    </div>
+</div>
+<div class="row row4">
+    <h2 class="text-center">Vestibulum Commodo Felis</h2>
+    <h3 class="text-center">Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna</h3>
+    <?php
+    $cat_id=5;
+    $blog=  new WP_Query(array('cat'=>$cat_id,'posts_per_page' =>3)); ?>
+    <?php  if ( $blog->have_posts() ) :  ?>
+        <div class="col-md-6">
+            <?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
+                <div class="row row4-left-article-bottom">
+                    <div class="col-sm-2 col-xs-2 "><?php the_post_thumbnail(true,array('class' => " img-responsive")); ?></div>
                     <div class="col-sm-10 col-xs-10 row4-left-article">
-                        <h5 >Gravida Ultrice</h5>
-                        <span>14 Comments</span>
-                        <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
+                        <a href="<?php the_permalink()?>"><h5 ><?php the_title()?></h5></a>
+                        <span><?php comments_number('no comment', '1 comment', '% comments'); ?></span>
+                        <p><?php the_excerpt()?></p>
+                        <a href="<?php the_permalink()?>">read more &nbsp; <i class="glyphicon glyphicon-arrow-right"></i></a>
                     </div>
-                    </div>
-
-                   <!--  <div class="row row4-left-article-bottom">
-                        <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
-                        <div class="col-sm-10 col-xs-10 row4-left-article">
-                            <h5 >Gravida Ultrice</h5>
-                            <span>14 Comments</span>
-                            <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
-                        </div>
-                    </div> -->
-                    
-                    <!-- <div class="row row4-left-article-bottom">
-                        <div class="col-sm-2 col-xs-2 "><img src="<?php bloginfo('template_url')?>/image/kl1.png" alt="kalendar"></div>
-                        <div class="col-sm-10 col-xs-10 row4-left-article">
-                            <h5 >Gravida Ultrice</h5>
-                            <span>14 Comments</span>
-                            <p>Suspendisse ac urna. Etiam pellentesque mauris ut lectus nunc tellus ante, mattis eget, gravida vitae, ultricies atinvio</p>
-                        </div>
-                    </div> -->
-
-                </div>        
-                 
-
-                <div class=" col-md-6 ">
-
-                <div class="row article-bottom ">
-                <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
-                    <span>- John Doe</span>
-                </div><div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
                 </div>
+            <?php endwhile; ?>
+        </div>
+    <?php else:?>
+    <?php endif; ?>
 
-              <!--   <div class="row article-bottom ">
-                  <div class="col-sm-9 col-xs-10 row4-right-article ">Sed egestas, ante et vulputate volutpat, eros pede semperest, vitae luctus metus libero eu augue.Morbi purus libero,faucibus adipiscing, commodo quis, gravida id, est.Sed lectus. Praesent elementum hendrerit tortor. Sedsemper lorem at felis. Vestibulum volutpat, lacus a ul.<br/>
-                            <span>- John Doe</span></div>
-                  <div class="col-sm-3 col-xs-2"><img src="image/qs.png" class="img-responsive"  alt=""></div>
-                 </div> -->
-
-                </div>     
-      </div>
-          
+    <?php
+    $cat_id=6;
+    $excerpt=  new WP_Query(array('cat'=>$cat_id,'posts_per_page' =>6)); ?>
+    <?php  if ( $excerpt->have_posts() ) :?>
+        <div class="col-md-6">
+            <?php while ( $excerpt->have_posts() ) : $excerpt->the_post(); ?>
+                <div class="row article-bottom ">
+                    <div class="col-sm-9 col-xs-10 row4-right-article "><?php the_excerpt()?><br/>
+                        <span>-<?php the_author()?></span>
+                    </div><div class="col-sm-3 col-xs-2"><?php the_post_thumbnail(true,array('class' => "img-rounded img-responsive")); ?></div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    <?php else:?>
+    <?php endif; ?>
+</div>
  <?php get_footer();?>
